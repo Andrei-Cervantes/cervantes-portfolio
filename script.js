6e7e6a42ef7a16233a9947299e65c73ad47a693c
Andrei-Cervantes <andrei29.cervantes@gmail.com>
2024-01-14 23:15:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-const projects = {
-    // add projects here
-}
+const projects = [
+    {
+        id: 1,
+        title: "PADES Printing",
+        icon: "./assets/pades.png",
+        githubLink: "asd".
+        liveDemo: "asd",
+        videoPresentation: ""
+    },
+    {
+
+    }
+]
 
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
