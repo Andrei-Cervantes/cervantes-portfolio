2922ea45e6d51d9c7c0326a1a512a13ada926fbb
Andrei-Cervantes <andrei29.cervantes@gmail.com>
2024-02-28 15:14:57 +0800

    Redesigned Nav Bar

--- script.js
+++ script.js
@@ -65,12 +65,17 @@ const projects = [
     },
 ]
 
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
-}
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', function() {
+    // if (links.classList.contains("show-links")) {
+    //     links.classList.remove("show-links");
+    // } else {
+    //     links.classList.add("show-links");
+    // }
+    links.classList.toggle("show-links");
+});
 
 // ********** set date ************
 const date = document.getElementById("date");
